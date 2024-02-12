03d2044b145d078f65946274e7932e6582955b44
unknown <lajouxiuna@gmail.com>
2024-02-13 00:11:02 +0100

    rotation et changement scale

--- tp2/lib/screens/exercice2_screen.dart
+++ tp2/lib/screens/exercice2_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Exercice2 extends StatefulWidget {
   const Exercice2({super.key});
@@ -11,41 +10,99 @@ class Exercice2 extends StatefulWidget {
 }
 
 class _Ex2ScreenState extends State<Exercice2> {
-  double angleRotation = 0; //Angle de rotation déterminé par le slider
+  //double angleRotation = 0; //Angles de rotation déterminés par les sliders
+  double angleRotationX = 0;
+  double angleRotationZ = 0;
+  double dimension = 10; 
   @override
   Widget build(BuildContext context) {
-     
     return Scaffold(
         appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Exercice 2"),
-      ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Exercice 2"),
+        ),
         body: Column(
           children: [
-            Transform.rotate(angle : angleRotation, child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 30, 0, 8),
-              child: SizedBox(height :350, width :1000, child: Image.network('https://picsum.photos/512/1024.jpg')),
-            )),
+            Transform(
+                transform: Matrix4.rotationX(angleRotationX)
+                  ..rotateZ(angleRotationZ)..scale(dimension),
+                  alignment: Alignment.center,
+                //angle: angleRotation,
+                child: SizedBox(
+                    height: 350,
+                    width: 1000,
+                    child:
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 15, 0, 8),
+                          child: Image.network('https://picsum.photos/512/1024.jpg'),
+                        ))),
             Row(
               children: [
                 const Padding(
                   padding: EdgeInsets.all(8.0),
-                  child: Text("Rotation de l'image : "),
+                  child: Text("Rotation de l'image selon X : "),
                 ),
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Slider(value: angleRotation, min:0 , max:2*pi,  onChanged: (value) {setState(() {
-                      angleRotation = value; 
-                    });}),
+                    padding: const EdgeInsets.fromLTRB(8, 10, 8, 0),
+                    child: Slider(
+                        value: angleRotationX,
+                        min: 0,
+                        max: 2 * pi,
+                        onChanged: (value) {
+                          setState(() {
+                            angleRotationX = value;
+                          });
+                        }),
                   ),
                 ),
               ],
-            )
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("Rotation de l'image selon Z : "),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Slider(
+                        value: angleRotationZ,
+                        min: 0,
+                        max: 2 * pi,
+                        onChanged: (value) {
+                          setState(() {
+                            angleRotationZ = value;
+                          });
+                        }),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("Changement de taille : "),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Slider(
+                        value: dimension,
+                        min: 0.1,
+                        max: 10,
+                        onChanged: (value) {
+                          setState(() {
+                            dimension = value;
+                          });
+                        }),
+                  ),
+                ),
+              ],
+            ),
           ],
         ));
-
-
-    //throw UnimplementedError();
   }
 }
