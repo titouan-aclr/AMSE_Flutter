2c57a51ef7f9c0741292e137f66fbea609bbe7d8
IunaLaj <lajouxiuna@gmail.com>
2024-02-19 17:51:32 +0100

    modification deplacement tile

--- tp2/lib/screens/exercice7_screen.dart
+++ tp2/lib/screens/exercice7_screen.dart
@@ -45,34 +45,36 @@ class Exercice7Screen extends StatefulWidget {
 
 class _Exercice7ScreenState extends State<Exercice7Screen> {
   int nbColumns = 3;
-  late int itemCount;
+  late int itemCount = 9;
   int indexEmpty = 1;
   late List<Widget> tiles;
   TileWidget emptyTile = TileWidget(tile: Tile(Colors.white, "Empty"));
 
   @override
   void initState() {
-    itemCount = nbColumns * nbColumns;
-    super.initState();
-  }
-
-  List<Widget> populateList() {
-    List<Widget> tempTiles = List<Widget>.generate(
+    tiles = List<Widget>.generate(
         itemCount - 1,
         (index) =>
             TileWidget(tile: Tile(Colors.grey, "Tile ${index.toString()}")));
-    tempTiles.insert(indexEmpty, emptyTile);
-    return tempTiles;
+    tiles.insert(indexEmpty, emptyTile);
+    super.initState();
+  }
+
+  void populateList() {
+    tiles = List<Widget>.generate(
+        itemCount - 1,
+        (index) =>
+            TileWidget(tile: Tile(Colors.grey, "Tile ${index.toString()}")));
+    tiles.insert(indexEmpty, emptyTile);
   }
 
   @override
   Widget build(BuildContext context) {
-    tiles = populateList();
-
+    itemCount = nbColumns * nbColumns;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Invert two tiles with grid"),
+        title: const Text("Invert two tiles with sizable grid"),
       ),
       body: Column(
         children: [
@@ -104,10 +106,7 @@ class _Exercice7ScreenState extends State<Exercice7Screen> {
           ),
           Slider(
             value: nbColumns.toDouble(),
-            onChanged: (newNbColumns) => setState(() {
-              nbColumns = newNbColumns.toInt();
-              itemCount = nbColumns * nbColumns;
-            }),
+            onChanged: (newNbColumns) => changeNbColumns(newNbColumns.toInt()),
             min: 2,
             divisions: 6,
             max: 8,
@@ -115,14 +114,21 @@ class _Exercice7ScreenState extends State<Exercice7Screen> {
           )
         ],
       ),
-      bottomNavigationBar: ConvexAppBar(
-        items : const [
-          TabItem(icon: Icons.add_circle_outline),
-          TabItem(icon: Icons.play_arrow_outlined),
-          TabItem(icon: Icons.remove_circle_outline),
-        ]
-      ),
+      bottomNavigationBar: ConvexAppBar(items: const [
+        TabItem(icon: Icons.add_circle_outline),
+        TabItem(icon: Icons.play_arrow_outlined),
+        TabItem(icon: Icons.remove_circle_outline),
+      ]),
     );
+  }
+
+  changeNbColumns(int newNbColumns) {
+    setState(() {
+      nbColumns = newNbColumns;
+      itemCount = nbColumns * nbColumns;
+      if (indexEmpty >= itemCount) indexEmpty = 0;
+    });
+    populateList();
   }
 
   swapTiles(int index) {
